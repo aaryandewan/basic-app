edc2ff2d1ade0dc6aa03d9484238842a59b68589
Aaryan Dewan <aaryandewan@yahoo.com>
2022-08-06 12:24:28 +0530

    basic working ok

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import HomePage from "./components/homePage";
+import SignIn from "./components/SignIn";
+import Button from "@mui/material/Button";
+import { Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
-      <div>
-        My first app
-      </div>
+    <div>
+      {/* <Button variant="contained">Hello World</Button>; */}
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="signin" element={<SignIn />} />
+      </Routes>
+    </div>
   );
 }
 
