79196bc093ed51ed1692038c515340904caab930
Aaryan Dewan <aaryandewan@yahoo.com>
2022-08-08 08:35:27 +0530

    add firebase

---
+++ src/firebase/init.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAmof-PHzuI_CVF8xivLEsdCilX5WLcCWk",
+  authDomain: "stproject-cd675.firebaseapp.com",
+  projectId: "stproject-cd675",
+  storageBucket: "stproject-cd675.appspot.com",
+  messagingSenderId: "163010623462",
+  appId: "1:163010623462:web:99fd192e2a61e6fd65c827",
+};
+
+const app = initializeApp(firebaseConfig);
