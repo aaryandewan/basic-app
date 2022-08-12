a25a9da96f6a96d5b6398116fcc0228942a7dd84
Aaryan Dewan <aaryandewan@yahoo.com>
2022-08-12 09:54:06 +0530

    Tryna add protected routes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,41 @@
+import { useEffect, useState } from "react";
 import HomePage from "./components/homePage";
 import SignIn from "./components/SignIn";
 import Button from "@mui/material/Button";
 import Signup from "./components/Signup";
-import { Routes, Route, Link } from "react-router-dom";
+import Profile from "./components/Profile";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+
+import { Routes, Route, Link, Navigate } from "react-router-dom";
 
 function App() {
+  const [currUser, setCurrUser] = useState(null);
+
+  const auth = getAuth();
+
+  const ProtectedRoute = ({ user, children }) => {
+    if (!currUser) {
+      return <Navigate to="/" replace />;
+    }
+
+    return children;
+  };
+
   return (
     <div>
-      {/* <Button variant="contained">Hello World</Button>; */}
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="signin" element={<SignIn />} />
         <Route path="signup" element={<Signup />} />
+        <Route
+          path="/profile"
+          element={
+            <ProtectedRoute user={currUser}>
+              <Profile />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="*" element={<p>There's nothing here: 404!</p>} />
       </Routes>
     </div>
   );
