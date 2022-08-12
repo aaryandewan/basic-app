dbc0012589770e54112e28ab63b7bffa69c7ce6a
Aaryan Dewan <aaryandewan@yahoo.com>
2022-08-12 10:01:39 +0530

    Private routing working

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import Signup from "./components/Signup";
 import Profile from "./components/Profile";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 
+import PrivateRoute from "./routing/PrivateRoute";
+
 import { Routes, Route, Link, Navigate } from "react-router-dom";
 
 function App() {
@@ -27,14 +29,18 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="signin" element={<SignIn />} />
         <Route path="signup" element={<Signup />} />
-        <Route
+        {/* <Route
           path="/profile"
           element={
             <ProtectedRoute user={currUser}>
               <Profile />
             </ProtectedRoute>
           }
-        />
+        /> */}
+        <Route element={<PrivateRoute />}>
+          <Route path="/profile" element={<Profile />} />
+        </Route>
+
         <Route path="*" element={<p>There's nothing here: 404!</p>} />
       </Routes>
     </div>
---
+++ src/routing/PrivateRoute.js
@@ -0,0 +1,38 @@
+import { useEffect, useState } from "react";
+import { Outlet, Navigate } from "react-router-dom"; // v4/5
+import { onAuthStateChanged } from "firebase/auth";
+import { getAuth } from "firebase/auth";
+
+// const auth = getAuth();
+
+const PrivateRoute = (props) => {
+  const [pending, setPending] = useState(true);
+  const [currentUser, setCurrentUser] = useState();
+
+  useEffect(() => {
+    const auth = getAuth();
+    const unsubscribe = onAuthStateChanged(
+      auth,
+      (user) => {
+        setCurrentUser(user);
+        setPending(false);
+      },
+      (error) => {
+        // any error logging, etc...
+        setPending(false);
+      }
+    );
+
+    return unsubscribe; // <-- clean up subscription
+  }, []);
+
+  if (pending) return null; // don't do anything yet
+
+  return currentUser ? (
+    <Outlet /> // <-- render outlet for routes
+  ) : (
+    <Navigate to="/" replace />
+  ); // <-- redirect to log in
+};
+
+export default PrivateRoute;
