4479c849902bbe3b948f39cb77452aedbdcfa341
Aaryan Dewan <aaryandewan@yahoo.com>
2022-08-12 10:04:19 +0530

    clean

--- src/App.js
+++ src/App.js
@@ -1,28 +1,13 @@
-import { useEffect, useState } from "react";
 import HomePage from "./components/homePage";
 import SignIn from "./components/SignIn";
-import Button from "@mui/material/Button";
 import Signup from "./components/Signup";
 import Profile from "./components/Profile";
-import { getAuth, onAuthStateChanged } from "firebase/auth";
 
 import PrivateRoute from "./routing/PrivateRoute";
 
-import { Routes, Route, Link, Navigate } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
-  const [currUser, setCurrUser] = useState(null);
-
-  const auth = getAuth();
-
-  const ProtectedRoute = ({ user, children }) => {
-    if (!currUser) {
-      return <Navigate to="/" replace />;
-    }
-
-    return children;
-  };
-
   return (
     <div>
       <Routes>
