bba3db417ca9abfa8057dd57506694a806b9abfc
Aaryan Dewan <aaryandewan@yahoo.com>
2022-08-12 11:53:54 +0530

    ITS WORKING!!!!!!

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -2,13 +2,34 @@ import { useState, useEffect } from "react";
 import { getAuth } from "firebase/auth";
 import { getStorage, ref, uploadBytes } from "firebase/storage";
 
+import * as React from "react";
+import Stack from "@mui/material/Stack";
+import Snackbar from "@mui/material/Snackbar";
+import MuiAlert from "@mui/material/Alert";
+
 import Button from "@mui/material/Button";
 
 const storage = getStorage();
 
 export default function UploadButtons() {
-  const [image, setImage] = useState(null);
+  const [open, setOpen] = React.useState(false);
 
+  const [image, setImage] = useState(null);
+  const handleClick = () => {
+    setOpen(true);
+  };
+
+  const handleClose = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
+
+    setOpen(false);
+  };
+
+  const Alert = React.forwardRef(function Alert(props, ref) {
+    return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+  });
   const upload = () => {
     if (image == null) return;
     else console.log("image not null");
@@ -27,6 +48,7 @@ export default function UploadButtons() {
       const imagesRef = ref(storage, fString);
       uploadBytes(imagesRef, image).then((snapshot) => {
         console.log("Uploaded a blob or file!");
+        handleClick();
       });
 
       console.log("ZYZZZZZZ", imagesRef);
@@ -40,15 +62,22 @@ export default function UploadButtons() {
   }, [image]);
 
   return (
-    <Button variant="contained" component="label" onClick={upload}>
-      Upload File
-      <input
-        type="file"
-        onChange={(e) => {
-          setImage(e.target.files[0]);
-        }}
-        hidden
-      />
-    </Button>
+    <div>
+      <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
+        <Alert onClose={handleClose} severity="success" sx={{ width: "100%" }}>
+          Image Uploaded!
+        </Alert>
+      </Snackbar>
+      <Button variant="contained" component="label" onClick={upload}>
+        Upload File
+        <input
+          type="file"
+          onChange={(e) => {
+            setImage(e.target.files[0]);
+          }}
+          hidden
+        />
+      </Button>
+    </div>
   );
 }
