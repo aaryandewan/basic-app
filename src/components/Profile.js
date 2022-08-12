dae52c69136d2bce53dd9d5ab1b0504c8a5f8df3
Aaryan Dewan <aaryandewan@yahoo.com>
2022-08-12 11:14:38 +0530

    kk

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,7 +1,54 @@
-import React from "react";
+import { useState, useEffect } from "react";
+import { getAuth } from "firebase/auth";
+import { getStorage, ref, uploadBytes } from "firebase/storage";
 
-function Profile() {
-  return <div>Profile</div>;
+import Button from "@mui/material/Button";
+
+const storage = getStorage();
+
+export default function UploadButtons() {
+  const [image, setImage] = useState(null);
+
+  const upload = () => {
+    if (image == null) return;
+    else console.log("image not null");
+    // storage.ref(`/images/${image.name}`).put(image)
+    // .on("state_changed" , alert("success") , alert);
+  };
+
+  useEffect(() => {
+    const auth = getAuth();
+    const user = auth.currentUser;
+    if (user && image) {
+      //   console.log("In the true clause");
+      let fString = "images/".concat(user.uid);
+      fString.concat(".jpg");
+
+      const imagesRef = ref(storage, fString);
+      uploadBytes(imagesRef, image).then((snapshot) => {
+        console.log("Uploaded a blob or file!");
+      });
+
+      console.log("ZYZZZZZZ", imagesRef);
+
+      console.log("Hey");
+      console.log(user.uid);
+    } else {
+      console.log("IN the else block");
+      // No user is signed in.
+    }
+  }, [image]);
+
+  return (
+    <Button variant="contained" component="label" onClick={upload}>
+      Upload File
+      <input
+        type="file"
+        onChange={(e) => {
+          setImage(e.target.files[0]);
+        }}
+        hidden
+      />
+    </Button>
+  );
 }
-
-export default Profile;
